c863d5460c09aaea986dc5ec705eec4312beeabe
lawrence <aceliuchanghong@gmail.com>
2024-06-10 18:05:07 +0800

    feat: 09增加

---
+++ 09-fun_tools/maze.py
@@ -0,0 +1,16 @@
+import mmaze
+
+# https://github.com/MorvanZhou/mmaze
+m = mmaze.generate(width=30, height=30)
+print(m)
+m.plot()
+
+"""
+||||||||||||||
+||S ********||
+||  ||||||**||
+||  ||    **||
+||  ||||||**||
+||  ||    E ||
+||||||||||||||
+"""
